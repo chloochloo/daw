0be3f7413501266380b7b5d05e0b3502b4998262
maxenceprsll <127317527+maxenceprsll@users.noreply.github.com>
2024-03-14 22:21:56 +0100

    v0.0.1
    Update deconnexion.php

---
+++ ploodle/php/deconnexion.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'bibli_generale.php';
+
+head('Deconnexion');
+nav();
+footer();
+
+unset($_SESSION['usLogin']);
+unset($_SESSION['usID']);
+unset($_SESSION['usNom']);
+unset($_SESSION['usPrenom']);
+
+if(isset($_SERVER['HTTP_REFERER'])) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+} else {
+    header('Location: ../index.php');
+}
+
